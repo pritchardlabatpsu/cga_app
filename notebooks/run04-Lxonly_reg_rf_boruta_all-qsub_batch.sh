755702cd8b156e0365a34e083f43bbae0fb1958f
Boyang Zhao <bozhao@quantalarity.com>
2020-09-13 10:03:22 +0200

    minor fix to batch qsub

--- notebooks/run04-Lxonly_reg_rf_boruta_all-qsub_batch.sh
+++ notebooks/run04-Lxonly_reg_rf_boruta_all-qsub_batch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p cluster_job
+
 # Submit jobs to cluster
-qsub run04-Lxonly_reg_rf_boruta_all.sh 100
-qsub run04-Lxonly_reg_rf_boruta_all.sh 200
+qsub -cwd -N 100 -o cluster_job/o100 -e cluster_job/e100 run04-Lxonly_reg_rf_boruta_all.sh 100
+qsub -cwd -N 200 -o cluster_job/o200 -e cluster_job/e200 run04-Lxonly_reg_rf_boruta_all.sh 200
